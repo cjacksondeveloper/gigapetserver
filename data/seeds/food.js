5bfa283bb8221334bc92cbe05aa7f32dd30f17b2
Jacob Bryan <jtb36688@gmail.com>
2019-03-13 20:00:03 -0400

    Fixed addfood route

--- data/seeds/food.js
+++ data/seeds/food.js
@@ -1,16 +1,27 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex('food').truncate()
     .then(function () {
-      // Inserts seed entries
       return knex('food').insert([
-        {id: 1, foodName: "Pizza"},
-        {id: 2, foodName: "Baby Food"},
-        {id: 3, foodName: "Pancakes"},
-        {id: 4, foodName: "Salad"},
-        {id: 5, foodName: "Chicken"},
-        {id: 6, foodName: "Pancakes"},
+        {id: 1, foodName: "Salad", foodType: "Vegetable", mealTime:"Breakfast", childId: 1, date: "03_12_2019"},
+        {id: 2, foodName: "Oranges", foodType: "Fruit", mealTime:"Lunch", childId: 1, date: "03_12_2019"},
+        {id: 3, foodName: "Chicken", foodType: "Meat", mealTime:"Dinner", childId: 1, date: "03_12_2019"},
+        {id: 4, foodName: "Salad", foodType: "Vegetable", mealTime:"Breakfast", childId: 1, date: "03_13_2019"},
+        {id: 5, foodName: "Oranges", foodType: "Fruit", mealTime:"Lunch", childId: 1, date: "03_13_2019"},
+        {id: 6, foodName: "Chicken", foodType: "Meat", mealTime:"Dinner", childId: 1, date: "03_13_2019"},
+        {id: 7, foodName: "Salad", foodType: "Vegetable", mealTime:"Breakfast", childId: 2, date: "03_12_2019"},
+        {id: 8, foodName: "Oranges", foodType: "Fruit", mealTime:"Lunch", childId: 2, date: "03_12_2019"},
+        {id: 9, foodName: "Chicken", foodType: "Meat", mealTime:"Dinner", childId: 2, date: "03_12_2019"},
+        {id: 10, foodName: "Salad", foodType: "Vegetable", mealTime:"Breakfast", childId: 2, date: "03_13_2019"},
+        {id: 11, foodName: "Oranges", foodType: "Fruit", mealTime:"Lunch", childId: 2, date: "03_13_2019"},
+        {id: 12, foodName: "Chicken", foodType: "Meat", mealTime:"Dinner", childId: 2, date: "03_13_2019"},
+        {id: 13, foodName: "Salad", foodType: "Vegetable", mealTime:"Breakfast", childId: 3, date: "03_12_2019"},
+        {id: 14, foodName: "Oranges", foodType: "Fruit", mealTime:"Lunch", childId: 3, date: "03_12_2019"},
+        {id: 15, foodName: "Chicken", foodType: "Meat", mealTime:"Dinner", childId: 3, date: "03_12_2019"},
+        {id: 16, foodName: "Salad", foodType: "Vegetable", mealTime:"Breakfast", childId: 3, date: "03_13_2019"},
+        {id: 17, foodName: "Oranges", foodType: "Fruit", mealTime:"Lunch", childId: 3, date: "03_13_2019"},
+        {id: 18, foodName: "Chicken", foodType: "Meat", mealTime:"Dinner", childId: 3, date: "03_13_2019"},
+        {id: 19, foodName: "Salad", foodType: "Vegetable", mealTime:"Breakfast", childId: 4, date: "03_12_2019"}
       ]);
     });
 };
